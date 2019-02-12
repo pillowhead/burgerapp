b1c5bcd50ff3b72078bdccd8b1a675f3a104e9a8
pillowhead <gabriel.belmonte@gmail.com>
2019-02-12 22:32:15 +0100

    1st build attempt

---
+++ build/precache-manifest.88405208f228d048672de319da8d094b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79ef85769ec05f935cbd",
+    "url": "/static/js/main.79ef8576.chunk.js"
+  },
+  {
+    "revision": "f24cba37d145848e0b52",
+    "url": "/static/js/1.f24cba37.chunk.js"
+  },
+  {
+    "revision": "79ef85769ec05f935cbd",
+    "url": "/static/css/main.25d8351a.chunk.css"
+  },
+  {
+    "revision": "21a72d6b1becb48008f952859d8e9852",
+    "url": "/index.html"
+  }
+];
